b3c925348a96068fcbfd8ca4542e9aaa2e87a468
ninek_flames <cheberiak.av@gmail.com>
2018-10-28 03:06:07 +0200

    Implement dropdown mechanism for scream bar

--- src/ScreamBar.jsx
+++ src/ScreamBar.jsx
@@ -1,16 +1,37 @@
 import React, {Component} from "react";
 
 export class ScreamBar extends Component {
+    constructor(state) {
+        super(state);
+        this.state = {
+            showScreamInputForm: false
+        }
+    }
+
     render() {
         return (
-            <div style={ScreamBar.styles.topBar}>
+            <div style={this.state.showScreamInputForm
+                ? ScreamBar.styles.topBarHigh
+                : ScreamBar.styles.topBarShort}
+                 onBlur={() => {
+                     this.setState({showScreamInputForm: false});
+                 }}
+            >
                 <div style={ScreamBar.styles.textInputWrapper}>
-                    <input style={ScreamBar.styles.textInput}
-                           onClick={(e) => {
-                               e.stopPropagation();
-                               alert("test")
+                    <input type="text"
+                           style={ScreamBar.styles.textInput}
+                           placeholder="AAAAAAAAAAAAa"
+                           onFocus={() => {
+                               this.setState({showScreamInputForm: true});
                            }}
-                    />
+                           onBlur={(e) => e.stopPropagation()}/>
+                    {this.state.showScreamInputForm
+                        ? (
+                            <input type="text"
+                                   style={ScreamBar.styles.textInput}
+                                   placeholder="AAAAAAAAAAAAa"/>
+                        )
+                        : null}
                 </div>
                 <button style={ScreamBar.styles.topBarButton}>SCREAM</button>
             </div>
@@ -20,15 +41,22 @@ export class ScreamBar extends Component {
 
 ScreamBar.styles = {
     textInput: {
-        "type": "text",
         "background-color": "#e5e5e5",
-        "height": "100%",
-        "width": "100%",
+        "width": "80%",
         "outline": "none",
-        "margin-left": "2%",
-        "border-width": "0",
-        "border-radius": "6px 0 0 6px",
-        "box-sizing": "border-box"
+        "font-size": "1rem",
+        "margin": "10px 10px 10px 10px",
+        "border": "0",
+        "border-bottom": "2px",
+        "border-style": "solid",
+        "border-color": "#d3d3d3",
+        "box-sizing": "border-box",
+        ":focus": {
+            "border": "0",
+            "border-bottom": "2px",
+            "border-style": "solid",
+            "border-color": "#3770c6"
+        }
     },
     textInputWrapper: {
         "border-width": "1px 0 1px 1px",
@@ -58,7 +86,20 @@ ScreamBar.styles = {
             "background-color": "#3770c6"
         }
     },
-    topBar: {
+    topBarHigh: {
+        "position": "absolute",
+        "align": "justify",
+        "top": "5%",
+        "left": "50%",
+        "transform": "translate(-50%)",
+        "width": "70%",
+        "height": "15rem",
+        "display": "flex",
+        "justify-content": "space-between",
+        "align-items": "center",
+        "padding": "0"
+    },
+    topBarShort: {
         "position": "absolute",
         "align": "justify",
         "top": "5%",
